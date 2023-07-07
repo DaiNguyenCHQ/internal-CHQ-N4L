6d0590d1fb14d2fb70a99aa97ea4a23481db35d1
Dai Nguyen <dai.nguyen@codehq.nz>
2023-07-07 14:58:34 +0700

    add needed packages

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { TodosComponent } from './todos/todos.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodosComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HeaderComponent,
+    FooterComponent
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
